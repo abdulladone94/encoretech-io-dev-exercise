812b29057d0bcf0f70e7047d4e1ab99918f3f05e
abdulladone94 <abdulladone94@gmail.com>
2021-12-08 08:30:08 +0530

    React router dom to navigate

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import SearchAppBar from "./SearchBar";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  // Switch,
+  // Route,
+  Link,
+  // useRouteMatch,
+  // useParams,
+} from "react-router-dom";
 
 export default function App() {
   const [fetchData, setFetchData] = useState("");
@@ -14,19 +22,22 @@ export default function App() {
   if (!fetchData) return null;
 
   return (
-    <div>
-      <SearchAppBar />
-      {fetchData.map((card) => {
-        return (
-          <div className="note">
-            <h1>
-              {card.id + ".   "}
-              {card.title}
-            </h1>
-            <p>{card.body}</p>
-          </div>
-        );
-      })}
-    </div>
+    <Router>
+      <div>
+        <SearchAppBar />
+        {fetchData.map((card) => {
+          return (
+            <div className="note">
+              <h1>
+                {card.id + ".   "}
+                {card.title}
+              </h1>
+              <p>{card.body}</p>
+              <Link>View More</Link>
+            </div>
+          );
+        })}
+      </div>
+    </Router>
   );
 }
