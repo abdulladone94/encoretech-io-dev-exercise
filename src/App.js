870e6bde9a47dc5c4dae51392dd21eabf9ef02db
abdulladone94 <abdulladone94@gmail.com>
2021-12-08 07:35:36 +0530

    Using Axios fech & display data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import SearchAppBar from "./SearchBar";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [fetchData, setFetchData] = useState("");
+
+  useEffect(() => {
+    axios.get("http://jsonplaceholder.typicode.com/posts").then((response) => {
+      setFetchData(response.data);
+    });
+  }, []);
+  if (!fetchData) return null;
+
   return (
-    <div className="App">
+    <div>
       <SearchAppBar />
+      {fetchData.map((card) => {
+        return (
+          <div>
+            <h1>
+              {card.id + ".   "}
+              {card.title}
+            </h1>
+            <p>{card.body}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
